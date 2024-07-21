8e0ecc6f9694d09fcfb2fae6f0a9c864b173c59f
Yunus-uk <yunuskhansia@yahoo.co.uk>
2024-07-21 22:40:35 +0100

    Changes

--- server.js
+++ server.js
@@ -5,4 +5,16 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/tickets", (req, res) => {
+  res.send("Job done");
+});
+
+app.put("/submitted", (req, res) => {
+  res.send("/submitted");
+});
+
+app.patch("/updated", (req, res) => {
+  res.send("/updated");
+});
+
 app.listen(3000);
